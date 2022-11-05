d863569a68c6ba90a2cfa0a0f76250ad7d700e08
Peter Costescu <peter15@qrts.com>
2022-11-04 21:01:12 -0400

    minor comment fix

--- kiss_transmit.cpp
+++ kiss_transmit.cpp
@@ -28,8 +28,8 @@ int main(int argc, char *argv[])
 
     cout << "Connection succesful." << endl;
 
-    const char *msg = "NASA SLVT"; // 9 bytes
     const char *call = "KQ4DIB"; // 6 bytes
+    const char *msg = "NASA SLVT"; // 9 bytes
     const char FEND = 0xc0; 
     const char DATA = 0x00;
 
@@ -37,8 +37,8 @@ int main(int argc, char *argv[])
     // total 18 bytes
     // message looks like following (ASCII chars used instead of hex for message body)
     // remember, 0x20 is space
-    // 0xc0 0x00 K Q 4 D I B N A S A 0x20 S L V T
-    
+    // 0xc0 0x00 K Q 4 D I B N A S A 0x20 S L V T 0xc0
+
     char buffer[256];
     memset(buffer, 0, sizeof(char) * 256);
 
