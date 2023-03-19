44fa7fbd69ae0a0e98b2acf65522066ffbece311
Harshvardhan Sahay <bhvima@gmail.com>
2023-03-19 12:50:36 -0400

    servo

--- beagle/servo.c
+++ beagle/servo.c
@@ -22,14 +22,15 @@
 
 int init_servo(struct servo *servo) {
     BBIO_err err;
+    int fd;
 
     err = pwm_start(servo->ctrl_pin, 7.5, 50, 0);
     if (err != BBIO_OK) {
         syslog(LOG_ERR, "Servo: pwm_start failed");
-        return -1;
+        return 1;
     }
 
-    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    fd = open("/dev/mem", O_RDWR | O_SYNC);
     if (fd == -1) {
         syslog(LOG_ERR, "Servo: could not open /dev/mem");
         return 1;
@@ -42,7 +43,6 @@ int init_servo(struct servo *servo) {
     }
 
     servo->pru_dram = servo->pru + ((servo->prunum ? PRU1_DRAM : PRU0_DRAM) + 0x200)/4;
-
     close(fd);
 
     syslog(LOG_DEBUG, "Servo: initialize_servo: OK");
@@ -50,7 +50,6 @@ int init_servo(struct servo *servo) {
 }
 
 double servo_position(struct servo *servo) {
-
     double high_time = (double)servo->pru_dram[0];
     double total_time = (double)servo->pru_dram[1];
 
@@ -59,9 +58,10 @@ double servo_position(struct servo *servo) {
     double duty_cycle = high_time/total_time * 100;
     double angle = (duty_cycle - DCMIN) * 360 / (DCMAX - DCMIN + 1);
 
-    angle = (angle - servo->zero) % 360;
+    angle = angle - servo->zero;
+    angle = (angle < 0) ? 360 + angle : angle;
+    angle = (angle > 360) ? 360 : angle;
 
-    printf("%f\n", angle);
     return angle;
 }
 
@@ -71,62 +71,38 @@ void servo_zero(struct servo *servo) {
     syslog(LOG_DEBUG, "Servo: servo_zero: %f\n", servo->zero);
 }
 
-/*void servo_rotate(struct servo *servo, double value) {
-
-    double error = 0, prev_error = 0, output = 0;
-    clock_t start_time = clock();
-
-    while (1) {
-
-        error = servo_position(servo) - value;
-        if (error > 180) {
-            error = -360 + error;
-        } else if (error < -180) {
-            error = 360 + error;
-        }
-
-        if (abs(error) <= 0.01) {
-            servo_set_speed(servo, 0);
-            start_time = clock();
-            while (clock() - start_time < 0.1 * CLOCKS_PER_SEC);
-
-            if (abs(servo_position(servo) - value) < 0.01) break;
-        }
-
-        if (clock() - start_time >= 0.1 * CLOCKS_PER_SEC)
-        {
-            if (abs(error - prev_error) < 0.01) {
-                if (error > 0) { if (output > 0) output += 1; else output = 1;}
-                if (error < 0) { if (output < 0) output -= 1; else output = -1;}
-                servo_set_speed(servo, output);
-            } else {
-                output = 0;
-            }
-
-            prev_error = error;
-            start_time = clock();
-        }
-    }
-}*/
-
 void servo_set_speed(struct servo *servo, double speed) {
+    double duty_cycle;
 
     if (speed > MAXSPEED) speed = MAXSPEED;
     if (speed < -MAXSPEED) speed = -MAXSPEED;
 
-    if (speed == 0.0) {
-        pwm_set_duty_cycle(servo->ctrl_pin, 7.5);
-    } else if (speed < 0.0) {
-        pwm_set_duty_cycle(servo->ctrl_pin, 7.4 + (speed/140.0));
+    if (speed == 0) {
+        duty_cycle = 7.5;
+    } else if (speed < 0) {
+        duty_cycle = 7.4 + (speed/140.0);
     } else {
-        pwm_set_duty_cycle(servo->ctrl_pin, 7.6 + (speed/140.0));
+        duty_cycle = 7.6 + (speed/140.0);
     }
+
+    pwm_set_duty_cycle(servo->ctrl_pin, duty_cycle);
 }
 
 int servo_cleanup(struct servo *servo) {
+    int status = 0;
 
-    pwm_disable(servo->ctrl_pin);
-    munmap(servo->pru, PRU_LEN);
+    status = pwm_disable(servo->ctrl_pin);
+    if (status != 0) {
+        syslog(LOG_ERR, "Servo: servo_cleanup: pwm_disable failed");
+        return 1;
+    }
+
+    status = munmap(servo->pru, PRU_LEN);
+    if (status != 0) {
+        syslog(LOG_ERR, "Servo: servo_cleanup: munmap failed");
+        return 1;
+    }
+
     syslog(LOG_DEBUG, "Servo: close_servo: OK");
     return 0;
 }
--- beagle/servo.h
+++ beagle/servo.h
@@ -1,20 +1,29 @@
 #ifndef SERVO_H
 #define SERVO_H
 
+// Struct to store servo information
 struct servo {
-    uint32_t *pru;
-    uint32_t *pru_dram;
-    const char *ctrl_pin;
-    int prunum;
-    double zero;
+    uint32_t *pru; // Pointer to PRU memory
+    uint32_t *pru_dram; // Pointer to PRU DRAM memory
+    const char *ctrl_pin; // Control pin for the servo
+    int prunum; // PRU number
+    double zero; // Zero position of the servo
 };
 typedef struct servo servo;
 
+// Initialize the servo
 int init_servo(struct servo *servo);
+
+// Set the current position as its zero position
 void servo_zero(struct servo *servo);
-//void servo_rotate(struct servo *servo, double value);
+
+// Set the speed of the servo
 void servo_set_speed(struct servo *servo, double speed);
+
+// Get the current position of the servo
 double servo_position(struct servo *servo);
+
+// Cleanup the servo
 int servo_cleanup(struct servo *servo);
 
 #endif
